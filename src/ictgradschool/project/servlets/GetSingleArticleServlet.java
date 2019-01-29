d269633981cdc043a7ce6becf10f7e2f71ac5d01
Stephanie Hope <smh30@students.waikato.ac.nz>
2019-01-30 10:10:48 +1300

    created getSingleArticle servlet and page. very rough, needs more work

--- src/ictgradschool/project/servlets/GetSingleArticleServlet.java
+++ src/ictgradschool/project/servlets/GetSingleArticleServlet.java
@@ -1,11 +1,16 @@
 package ictgradschool.project.servlets;
 
+import ictgradschool.project.DAOs.ArticleDAO;
+import ictgradschool.project.JavaBeans.Article;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "GetSingleArticleServlet")
 public class GetSingleArticleServlet extends HttpServlet {
@@ -13,7 +18,23 @@ public class GetSingleArticleServlet extends HttpServlet {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("in the single article servlet");
+
+        Article article = new Article();
+
+        String currentUser = (String)(request.getSession().getAttribute("username"));
+       // String author = request.getParameter("author");
+        System.out.println("getting newest article by " + currentUser);
+
+        article = ArticleDAO.getSingleArticle(currentUser, getServletContext());
+        // get articles by that author
+
+
+        request.setAttribute("article", article);
+
+        request.getRequestDispatcher("web-pages/single-article.jsp").forward(request, response);
     }
+
 }
