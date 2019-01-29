dadfffd35721b400a41536da2f40d3c0f8608a89
Yasmin Brinkworth <yab2@students.waikato.ac.nz>
2019-01-29 16:29:32 +1300

    Bit confused here. Don't want all photos to print out, only the option of them... Will get second opinion from Steph and/or Yun.

--- src/ictgradschool/project/servlets/UploadProfilePictureServlet.java
+++ src/ictgradschool/project/servlets/UploadProfilePictureServlet.java
@@ -1,10 +1,12 @@
 package ictgradschool.project.servlets;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 @WebServlet(name = "UploadProfilePictureServlet")
@@ -17,6 +19,21 @@ public class UploadProfilePictureServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletContext servletContext = getServletContext();
+        String fullPhotoPath = servletContext.getRealPath("/default-photos-for-profile-page");
+
+        File pFolder = new File(fullPhotoPath);
+        File[] photoList = pFolder.listFiles();
+
+        if (photoList.length < 1) {
+            System.out.println("No photos stored in folder.");
+        } else {
+            for (File aPhotoList : photoList) {
+                   //TODO: Go through and print out photos onto page?????
+            }
+        }
+
         System.out.println("In the Upload Profile Picture Servlet, doGet");
         request.getRequestDispatcher("webpages/profile.jsp").forward(request, response);
     }
