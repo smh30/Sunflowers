06fe7c440f529edfb2e4ad0ae5127db847d110e3
Yasmin Brinkworth <yab2@students.waikato.ac.nz>
2019-02-07 11:31:00 +1300

    Admin can now add user!

--- src/ictgradschool/project/servlets/admin/AdminAddUserServlet.java
+++ src/ictgradschool/project/servlets/admin/AdminAddUserServlet.java
@@ -1,5 +1,7 @@
 package ictgradschool.project.servlets.admin;
 
+import ictgradschool.project.DAOs.UserDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +12,42 @@ import java.io.IOException;
 @WebServlet(name = "AdminAddUserServlet")
 public class AdminAddUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("in the AdminAddUser servlet POST");
+        System.out.println("Processing username and password");
+
+         /*In the Register servlet POST method, retrieve the username and password parameters
+        supplied.*/
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        boolean userCreated = UserDAO.newUser(username, password, getServletContext());
+        if (!userCreated){
+            String message = "please choose a different username";
+            request.setAttribute("message", message);
+
+            request.getRequestDispatcher("web-pages/register.jsp").forward(request,
+                    response);
+        } else {
+
+            System.out.println("Process other account details");
+
+            String country = request.getParameter("country");
+            String realName = request.getParameter("realname");
+            String description = request.getParameter("description");
+            String dateOfBirth = request.getParameter("dateofbirth");
+            String email = request.getParameter("email");
+
+            System.out.print(username);
+            System.out.println(realName);
+
+            UserDAO.editUser(username, country, realName, description, dateOfBirth,
+                    email, getServletContext());
+
+            response.sendRedirect("/admininterface");
+        }
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
